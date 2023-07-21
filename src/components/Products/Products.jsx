55d9b4527e35d609b90b85c23029e94c81cc949e
MarcosAF9 <marcos_falcade1@hotmail.com>
2023-07-21 16:37:54 -0300

    component de loading criado

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,29 +3,34 @@ import fetchProducts from "../../api/fetchProducts";
 import styles from "./products.module.css";
 import ProductCard from "../ProductCard/ProductCard";
 import formatCurrency from "../../utils/formatCurrency";
+import Loading from "../Loading/Loading";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts("rtx3060ti").then((response) => {
       setProducts(response);
+      setLoading(false);
     });
   }, []);
 
   console.log(products);
 
   return (
-    <section className={styles.products}>
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          title={product.title}
-          price={formatCurrency(product.price, product.currency_id)}
-          thumb={product.thumbnail}
-        />
-      ))}
-    </section>
+    (loading && <Loading />) || (
+      <section className={styles.products}>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            price={formatCurrency(product.price, product.currency_id)}
+            thumb={product.thumbnail}
+          />
+        ))}
+      </section>
+    )
   );
 };
 
